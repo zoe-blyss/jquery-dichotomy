f5f56973ef73c34e14c92d4383fc1248d5389077
Zoe Blyss Alexander <zoealexande@gmail.com>
2020-01-07 11:24:15 -0600

    All Components Added
    Project completed prior to learning GitHub

---
+++ app.js
@@ -0,0 +1,6 @@
+$('.image1').on('click', function(){
+//  anything that is inside the {} is whats gonna happen when the click is recongized
+ // $('.dinosaur').addClass('right-round')
+ $('.image1').toggleClass('fly-away')
+
+}) //<-- this is the closure of the click handler
